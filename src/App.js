befae59712b60925c76aa4085695bf9ab99643dd
steve <steve.hartken@gmail.com>
2021-01-25 22:00:13 -0500

    #3 -- ul reset
    Padding in user agent, for hearts

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <p><a href="https://github.com/urgent/thisbounty.com" style={{ color: "#fff" }}>readme.md</a></p>
         <p><a href="https://docs.google.com/spreadsheets/d/1RSD6Kz04bKAGty_Z_gvSYc_Gu4QtAcTPTuvkK752mEw/edit?usp=sharing" style={{ color: "#fff" }}>Bounties</a></p>
-        <p><Lifebar life={3} max={3} /></p>
+        <Lifebar life={3} max={3} />
       </header>
     </div>
   );
--- src/component/Lifebar.tsx
+++ src/component/Lifebar.tsx
@@ -1,14 +1,28 @@
 import React, {ReactChildren} from 'react'
 import {Heart} from './Heart'
+import styled from "styled-components";
+
+export const Bar = styled.ul`
+    display: grid;
+    grid-template-columns: ${ (props: {count?:number}) => Array.from(Array(props.count), (i) => '1fr').join(' ')};
+    list-style-type: none;
+    grid-column-gap: 3px;
+    justify-self: center;
+    padding:0;
+`
+
+export const Unit = styled.li`
+    display: inline;
+`
 
 export function Lifebar({life, max, children}: {life:number, max:number, children:ReactChildren}) {
-    return <ul>
+    return <Bar count={max}>
         {Array.from(Array(life), (_, i) => (
-            <li key={i}><Heart fill={true} /></li>
+            <Unit key={i}><Heart fill={true} /></Unit>
         ))}
         {Array.from(Array(max - life), (_, i) => (
-            <li key={i}><Heart fill={true} /></li>
+            <Unit key={i}><Heart fill={true} /></Unit>
         ))}
         {children}
-    </ul>
+    </Bar>
 }
